77a4ce6b55857c994ea02f129008fa7c6885b12f
Joe Corall <jjc223@lehigh.edu>
2025-03-27 10:33:26 -0400

    lint

--- scripts/ci/lint.sh
+++ scripts/ci/lint.sh
@@ -2,11 +2,9 @@
 
 set -eou pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-. "${SCRIPT_DIR}"/init.sh
-
-docker exec "$ISLE_PHP_FPM" cp web/core/phpcs.xml.dist .
-docker exec "$ISLE_PHP_FPM" php vendor/bin/phpcs \
+cd codebase
+cp web/core/phpcs.xml.dist .
+php vendor/bin/phpcs \
     --standard=Drupal \
     --extensions=module,php,inc \
     web/modules/custom
