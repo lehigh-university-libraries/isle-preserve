0dd49434e2e3e9e2015ac9489e40a06203118338
Joe Corall <jjc223@lehigh.edu>
2025-04-02 14:53:45 -0400

    clear cache after CI cleanup

--- scripts/ci/cleanup.sh
+++ scripts/ci/cleanup.sh
@@ -4,3 +4,4 @@ set -eou pipefail
 
 docker exec lehigh-d10-drupal-1 drush scr scripts/ci/cleanup.php
 docker exec lehigh-d10-drupal-1 rm -rf private/derivatives/
+docker exec lehigh-d10-drupal-1 drush cr
