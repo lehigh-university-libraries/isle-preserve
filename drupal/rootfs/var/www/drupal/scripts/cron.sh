380571f5b13e8be1100a7795f085775ff7c8feac
Joe Corall <jjc223@lehigh.edu>
2025-04-02 15:30:15 -0400

    fixup pdf aggregation query

--- drupal/rootfs/var/www/drupal/scripts/cron.sh
+++ drupal/rootfs/var/www/drupal/scripts/cron.sh
@@ -4,6 +4,6 @@ set -eou pipefail
 
 DURATION=${DURATION:-600}
 while true; do
-  drush queue:run lehigh_islandora_events
+  drush --uri "$DRUPAL_DRUSH_URI" queue:run lehigh_islandora_events
   sleep "$DURATION"
 done
