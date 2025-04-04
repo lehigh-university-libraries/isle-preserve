4bc870024d03d9d499f0006cd622ea601fb3aed2
Joe Corall <jjc223@lehigh.edu>
2025-04-04 13:11:21 -0400

    do not need to test TLS here

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -25,7 +25,7 @@ echo "testing HA setup"
 echo "=============================================\n\n"
 
 echo "make sure drupal is online"
-curl -vsf "https://${DOMAIN}/" -o /dev/null
+curl -kvsf "https://${DOMAIN}/" -o /dev/null
 
 echo "bring drupal containers down"
 docker stop lehigh-d10-drupal-1 lehigh-d10-drupal-lehigh-1
@@ -33,9 +33,9 @@ docker stop lehigh-d10-drupal-1 lehigh-d10-drupal-lehigh-1
 sleep 5
 
 echo "Send request to drupal container which should fail"
-curl -vsf \
+curl -kvsf \
   -H "X-Forwarded-For: 128.180.1.1" \
   "https://${DOMAIN}/?cache-warmer=1" && exit 1 || echo "Failed as expected"
 
 # make sure static site is still serving content
-curl -vsf "https://${DOMAIN}/" -o /dev/null
+curl -kvsf "https://${DOMAIN}/" -o /dev/null
