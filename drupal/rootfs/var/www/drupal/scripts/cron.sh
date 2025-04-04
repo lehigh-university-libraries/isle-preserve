720edc34088938a0af368cd66fd1cb063c0263ac
Joe Corall <jjc223@lehigh.edu>
2025-04-04 08:29:49 -0400

    Add a static frontend and a lehigh-dedicated backend, too

--- drupal/rootfs/var/www/drupal/scripts/cron.sh
+++ drupal/rootfs/var/www/drupal/scripts/cron.sh
@@ -2,6 +2,26 @@
 
 set -eou pipefail
 
+echo "Copying static assets into temp so our static site has access"
+DIRS=(
+  "core"
+  "themes"
+  "modules"
+)
+for DIR in "${DIRS[@]}"; do
+  mkdir -p /tmp/web/$DIR
+  rsync -av \
+    --include='*/' \
+    --include='*.js' \
+    --include='*.css' \
+    --include='*.jpg' \
+    --include='*.png' \
+    --include='*.ico' \
+    --include='*.svg' \
+    --exclude='*' \
+  /var/www/drupal/web/$DIR/ /tmp/web/$DIR/ && ls -l /tmp/web
+done
+
 DURATION=${DURATION:-600}
 while true; do
   drush --uri "$DRUPAL_DRUSH_URI" queue:run lehigh_islandora_events
