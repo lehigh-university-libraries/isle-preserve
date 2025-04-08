bd550fb15ccc872ee3f1282a7dcda666860e58f7
Joe Corall <jjc223@lehigh.edu>
2025-04-08 10:25:00 -0400

    Fix static site config and make lehigh pool only available from internal domain (#28)

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -20,28 +20,53 @@ docker exec lehigh-d10-drupal-1 \
     --debug \
     --verbose"
 
-echo "\n\n============================================="
+echo -e "\n\n============================================="
 echo "testing HA setup"
-echo "=============================================\n\n"
+echo -e "=============================================\n\n"
 
 echo "make sure drupal is online"
-curl -ksf \
-  -H "X-Forwarded-For: 128.180.1.1" \
-  "https://${DOMAIN}/" -o /dev/null
+curl -ksf "https://${DOMAIN}/" -o /dev/null
 
 echo "bring drupal containers down"
-docker stop lehigh-d10-drupal-1 lehigh-d10-drupal-lehigh-1
+docker stop lehigh-d10-drupal-1
 
-sleep 5
+sleep 11
 
 echo "Send request to drupal container which should fail"
-curl -ksf \
-  -H "X-Forwarded-For: 128.180.1.1" \
-  "https://${DOMAIN}/?cache-warmer=1"  -o /dev/null \
-&& exit 1 || echo "Failed as expected"
+curl -ksf "https://${DOMAIN}/?foo=bar" -o /dev/null \
+  && exit 1 || echo "Failed as expected"
 
 echo "make sure static site is still serving content"
 curl -ksf "https://${DOMAIN}/" -o /dev/null
 
 echo "all is well. Bring containers back up"
-docker start lehigh-d10-drupal-1 lehigh-d10-drupal-lehigh-1
+docker start lehigh-d10-drupal-1
+
+echo "Checking site is still OK if static service is down"
+docker stop lehigh-d10-drupal-static-1
+
+sleep 11
+
+curl -ksf "https://${DOMAIN}/" -o /dev/null
+
+echo "all is well. Bring containers back up"
+docker start lehigh-d10-drupal-static-1
+
+echo "Ensuring redirects work"
+ensure_redirect() {
+  curl -svk \
+    -H "Host: $2" \
+    "$1" 2>&1 \
+  | grep -i "location: https://${DOMAIN}" > /dev/null
+}
+HOSTS=(
+  "digitalcollections.lib.lehigh.edu"
+  "preserve.lib.lehigh.edu"
+)
+for HOST in "${HOSTS[@]}"; do
+  ensure_redirect "https://${DOMAIN}" "$HOST"
+  echo "$HOST redirected to https://$DOMAIN"
+done
+
+ensure_redirect "http://${DOMAIN}" "$DOMAIN"
+echo "http redirected to https for ${DOMAIN}"
