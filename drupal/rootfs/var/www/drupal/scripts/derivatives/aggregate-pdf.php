8b21dcbe6252c77d4f07b622f7a654edb93a23a5
Joe Corall <jjc223@lehigh.edu>
2025-04-10 07:57:00 -0400

    only check for paged content items with children

--- drupal/rootfs/var/www/drupal/scripts/derivatives/aggregate-pdf.php
+++ drupal/rootfs/var/www/drupal/scripts/derivatives/aggregate-pdf.php
@@ -3,6 +3,7 @@
 $action_name = 'paged_content_created_aggregated_pdf';
 $sql = "SELECT m.entity_id
   FROM node__field_model m
+  INNER JOIN node__field_member_of mo ON field_member_of_target_id = m.entity_id
   WHERE field_model_target_id = 27
     AND m.entity_id NOT IN (
       SELECT field_media_of_target_id FROM media__field_media_of mo
