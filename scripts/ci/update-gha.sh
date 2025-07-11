d0d8383196f53bb7a925868c073cc53ea9e6a403
Joe Corall <joe@libops.io>
2025-07-11 14:25:09 -0400

    point to new runner location

--- scripts/ci/update-gha.sh
+++ scripts/ci/update-gha.sh
@@ -5,9 +5,6 @@ set -eou pipefail
 # make sure k8s svc token stays fresh
 /app/scripts/ci/k8s/token.sh
 
-RUNNER_CONTAINER="github-actions-runner"
-RUNNER_IMAGE="ghcr.io/lehigh-university-libraries/docker-builds/actions-runner:main"
-
 docker_compose() {
     docker compose \
       --env-file .env \
@@ -16,6 +13,14 @@ docker_compose() {
       -f docker-compose.wight.yaml \
       "$@"
 }
+
+# Our GitHub Actions runner docker image is maintained at https://github.com/lehigh-university-libraries/docker-builds
+# When that docker image gets updated, we want to automatically receive updates
+# we can not roll out those updates with GitHub PRs without weird timeouts happening
+# because GitHub runners container would be restarted on a rollout, causing the GitHub Action workflow to hang
+# so instead we just check for updates and handle those as they come
+RUNNER_CONTAINER="github-actions-runner"
+RUNNER_IMAGE=$(docker_compose config "$RUNNER_CONTAINER" --format json | jq -r '.services["'"${RUNNER_CONTAINER}"'"].image')
 
 ensure_idle() {
     if docker_compose logs --tail 1 "$RUNNER_CONTAINER" | grep -q "Running job"; then
