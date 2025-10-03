e4eeeb052b686b33ce065b2f281720b854df1b74
Joe Corall <jjc223@lehigh.edu>
2025-10-03 10:50:55 -0400

    Move merge pdf replay from 1d to 1h

--- drupal/rootfs/var/www/drupal/scripts/derivatives/aggregate-pdf.php
+++ drupal/rootfs/var/www/drupal/scripts/derivatives/aggregate-pdf.php
@@ -11,7 +11,7 @@ $sql = "SELECT m.entity_id
   INNER JOIN node__field_member_of mo ON field_member_of_target_id = m.entity_id
   INNER JOIN node_field_data n ON n.nid = m.entity_id
   WHERE field_model_target_id = 27
-    AND n.created < UNIX_TIMESTAMP() - 86400
+    AND n.created < UNIX_TIMESTAMP() - 3600
     AND m.entity_id NOT IN (
       SELECT field_media_of_target_id FROM media__field_media_of mo
       INNER JOIN media__field_media_use mu ON mu.entity_id = mo.entity_id
