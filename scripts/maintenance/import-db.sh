13dcc985994c3313bf9347999961e47a7068bde2
Joe Corall <jjc223@lehigh.edu>
2026-01-07 09:22:04 -0500

    Update import-db.sh

--- scripts/maintenance/import-db.sh
+++ scripts/maintenance/import-db.sh
@@ -2,12 +2,14 @@
 
 set -eou pipefail
 
+pushd /opt/islandora/d10_lehigh_agile
+
 if [ "$(hostname)" = "islandora-prod" ]; then
   echo "Not running from production server. Exiting"
   exit 1
 fi
 
-F="./tmp/drupal/drupal.sql"
+F="/opt/islandora/volumes/tmp/drupal/drupal.sql"
 
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
@@ -22,17 +24,19 @@ if [ -f "${F}" ]; then
   file_age=$((current_time - file_modification_time))
   if [[ $file_age -gt 43200 ]]; then
     echo "Removing old SQL file. It's ${file_age}s old"
-    rm "${F}"
+    sudo rm "${F}"
   fi
 fi
 
 if [ ! -f "${F}" ]; then
-  cd ./tmp/drupal
-  rm drupal.sql.gz drupal.sql || echo "Files didn't exist"
+  pushd /tmp
   scp islandora-prod.lib.lehigh.edu:"/opt/islandora/backups/$YEAR/$MONTH/$DAY/drupal.sql.gz" .
   gunzip drupal.sql.gz
   sudo mv drupal.sql /opt/islandora/volumes/tmp/drupal/
-  touch drupal.sql
+  popd
 fi
-docker exec lehigh-d10-drupal-1 drush sqlq --debug --file /tmp/drupal.sql
-docker exec lehigh-d10-drupal-1 drush cr
+
+docker compose exec drupal drush sqlq --debug --file /tmp/drupal.sql
+docker compose exec drupal drush cr
+
+popd
