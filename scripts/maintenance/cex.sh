7137bcabd82d44b48548167e5fdce02a54ca3768
Joe Corall <jjc223@lehigh.edu>
2026-02-20 16:27:10 -0500

    drush cex

--- scripts/maintenance/cex.sh
+++ scripts/maintenance/cex.sh
@@ -7,7 +7,8 @@ SERVER=$1
 ssh $SERVER "docker exec lehigh-d10-drupal-1 rm /tmp/config.tar.gz || echo config-does-not-exist"
 ssh $SERVER "docker exec lehigh-d10-drupal-1 drush cex -y"
 ssh $SERVER "docker exec lehigh-d10-drupal-1 tar -zcvf /tmp/config.tar.gz config"
-scp $SERVER:/opt/islandora/volumes/tmp/drupal/config.tar.gz .
+ssh $SERVER "docker cp lehigh-d10-drupal-1:/tmp/config.tar.gz \$HOME/"
+scp $SERVER:~/config.tar.gz .
 tar -zxvf config.tar.gz
 rm -rf codebase/config
 mv config codebase/
